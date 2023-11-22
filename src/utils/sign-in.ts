f4bc747309aa678128e5dea67ccca03f484ccbc0
Artem Samarin <iahugo@yandex.ru>
2023-11-22 17:42:52 +0300

    Фикс ориентации.

--- src/utils/sign-in.ts
+++ src/utils/sign-in.ts
@@ -2,11 +2,15 @@ import axios from 'axios'
 
 import { TelegramUserPayload } from '@/types/data'
 
-export const signIn = async (data: TelegramUserPayload) =>
-  axios
+export const signIn = async (data: TelegramUserPayload) => {
+  return axios
     .post(`https://darcr-api.anclaev.com/auth/sign-in`, {
       ...data,
       id: `${data.id}`,
       auth_date: `${data.auth_date}`,
     })
-    .then(({ data }) => console.log(data))
+    .then(({ data }) => {
+      alert(`Hi, ${data.username}!`)
+      console.log(data)
+    })
+}
