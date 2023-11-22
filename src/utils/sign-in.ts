be8ac5da9a3f835ce576b0ca4c36f38c8bdb8f09
Artem Samarin <iahugo@yandex.ru>
2023-11-22 15:38:57 +0300

    Фикс API.

--- src/utils/sign-in.ts
+++ src/utils/sign-in.ts
@@ -3,6 +3,4 @@ import axios from 'axios'
 import { TelegramUserPayload } from '@/types/data'
 
 export const signIn = async (data: TelegramUserPayload) =>
-  axios
-    .post(`${process.env.REACT_APP_API_URL!}/auth/sign-in`, data)
-    .then(({ data }) => console.log(data))
+  axios.post(`/auth/sign-in`, data).then(({ data }) => console.log(data))
