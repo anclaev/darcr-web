2a541130fcb7c3987c9decb8dcdbb5e27da80a6e
Artem Samarin <iahugo@yandex.ru>
2023-11-23 15:53:43 +0300

    Фикс CI.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from '@interfaces/env'
 
 export const environment: IEnvironment = {
   API_URL: 'http://localhost:3002/',
-  TELEGRAM_TOKEN: '6761458067:AAG6GX3nG_GX9aecozTtPchckUH76kEcG8I',
+  TELEGRAM_TOKEN: '',
 }
